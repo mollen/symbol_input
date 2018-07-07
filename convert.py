cef0ab197fc40525972d106afc7da4d2076ee964
Christopher Mollén <chris.mollen@gmail.com>
2018-07-07 22:18:23 +0200

    modify convert script to handle new naming convention of table files

--- convert.py
+++ convert.py
@@ -8,13 +8,16 @@ def writeIBUSTable(lines, outputFile):
         frequency = 1
         print(words[0], words[1].strip('\n'), frequency, file=outputFile, sep='\t')
 
-
 tableType = 'IBUS'
+language = 'sv_SE'
 
-with open('tabell') as f:
+tableFile = 'swedishTable'
+if language == 'sv_SE':
+    tableFile = 'swedishTable'
+
+with open('swedishTable') as f:
     lines = f.readlines()
     lines = list(line for line in lines if line.strip(' ').strip('\n') and (not line.startswith('#')))
-
 
 if(tableType == 'IBUS'):
     outputFile = 'ibusTable'
@@ -22,11 +25,15 @@ if(tableType == 'IBUS'):
     templateFile = 'ibusTableTemplate'
     
     for line in fileinput.input(templateFile):
-        
-        if line != '###INSERT_HERE###\n':
-            print(line.strip('\n'), file=outputFile)
-        else:
+        if line == '###INSERT_HERE###\n':
             writeIBUSTable(lines, outputFile)
+        elif line == 'LANGUAGES = sv_SE\n':
+            if(language == 'sv_SE'):
+                print('LANGUAGES = sv_SE', file=outputFile)
+            else:
+                print(line.strip('\n'), file=outputFile)
+        else:
+            print(line.strip('\n'), file=outputFile)
             
     outputFile.close()
     
